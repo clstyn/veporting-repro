5b4c225487a2dc4a225bf384053a8537e878c9b3
clstyn <fiorenzacelestyn@gmail.com>
2024-02-21 10:25:09 +0700

    feat(ui): charts in dashboard

---
+++ src/app/(with-bar)/dashboard/_piechart.js
@@ -0,0 +1,49 @@
+import { useEffect, useRef } from "react";
+import { Chart } from "chart.js";
+
+export default function PieChart() {
+  const pieChartRef = useRef(null);
+
+  useEffect(() => {
+    if (pieChartRef.current === null) {
+      const ctx = document.getElementById("pieChart").getContext("2d");
+      pieChartRef.current = new Chart(ctx, {
+        type: "pie",
+        data: {
+          labels: ["Penetration Testing", "Vulnerability Assessment"],
+          datasets: [
+            {
+              data: [34, 40],
+              borderColor: ["#FF9F2E", "#BE1D2D"],
+              backgroundColor: ["rgba(255, 159, 46, 1)", "rgb(190, 29, 45, 1)"],
+              borderWidth: 2,
+            },
+          ],
+        },
+        options: {
+          responsive: true,
+          plugins: {
+            legend: {
+              position: "bottom",
+            },
+          },
+        },
+      });
+    }
+
+    return () => {
+      if (pieChartRef.current !== null) {
+        pieChartRef.current.destroy();
+        pieChartRef.current = null;
+      }
+    };
+  }, []);
+
+  return (
+    <div className="w-full h-full flex mx-auto my-auto">
+      <div className="pt-0 rounded-xl w-full h-fit my-auto pb-2">
+        <canvas id="pieChart"></canvas>
+      </div>
+    </div>
+  );
+}
--- src/app/(with-bar)/dashboard/page.js
+++ src/app/(with-bar)/dashboard/page.js
@@ -1,9 +1,13 @@
 "use client";
 
-import { SiWindows } from "react-icons/si";
-import { HiMiniDocumentText } from "react-icons/hi2";
-import { IoTime } from "react-icons/io5";
 import clsx from "clsx";
+import { HiMiniDocumentText } from "react-icons/hi2";
+import { HiExclamationTriangle } from "react-icons/hi2";
+import { HiMiniDocumentCheck } from "react-icons/hi2";
+
+import LineChart from "@/app/(with-bar)/dashboard/_linechart";
+import PieChart from "@/app/(with-bar)/dashboard/_piechart";
+import Table from "@/app/ui/dashboard/table/table";
 
 const cardList = [
   {
@@ -11,21 +15,21 @@ const cardList = [
     title: "Total",
     value: 1000,
     iconColor: "bg-blue-500",
-    icon: SiWindows,
+    icon: HiMiniDocumentText,
   },
   {
     id: "card-2",
     title: "Pending",
     value: 1000,
     iconColor: "bg-orange-500",
-    icon: SiWindows,
+    icon: HiExclamationTriangle,
   },
   {
     id: "card-3",
     title: "Approved",
     value: 1000,
     iconColor: "bg-green-500",
-    icon: IoTime,
+    icon: HiMiniDocumentCheck,
   },
 ];
 
@@ -55,7 +59,7 @@ export default function Home() {
       {cardList.map((card) => (
         <Card
           title={card.title}
-          icon={card.icon}
+          Icon={card.icon}
           value={card.value}
           key={card.id}
           iconColor={card.iconColor}
@@ -63,16 +67,20 @@ export default function Home() {
       ))}
 
       {/* Chart */}
-      <div className="p-6 font-semibold text-lg col-span-2 h-16 bg-white rounded-xl">
+      <div className="p-6 font-semibold text-lg col-span-2 bg-white rounded-xl">
         <p>Pertumbuhan Report</p>
+        <LineChart />
       </div>
-      <div className="p-6 font-semibold text-lg h-16 bg-white rounded-xl">
+
+      <div className="p-6 font-semibold text-lg bg-white rounded-xl">
         <p>Jenis Report</p>
+        <PieChart />
       </div>
 
       {/* Tabel */}
-      <div className="p-6 font-semibold text-lg col-span-3 h-16 bg-white rounded-xl">
-        Ongoing Project
+      <div className="p-6 text-lg col-span-3 bg-white rounded-xl">
+        <p className="font-semibold">Ongoing Project</p>
+        {/* <Table /> */}
       </div>
     </div>
   );
@@ -85,8 +93,13 @@ const Card = ({ title = "", value = 0, Icon = null, iconColor }) => {
         <p className="text-sm">{title}</p>
         <p className="text-3xl">{value}</p>
       </div>
-      <div className={clsx(iconColor, "h-full aspect-square rounded-full")}>
-        {/* <Icon size={24} /> */}
+      <div
+        className={clsx(
+          iconColor,
+          "h-full aspect-square rounded-full flex items-center justify-center w-16 text-white"
+        )}
+      >
+        <Icon size={38} />
       </div>
     </div>
   );
