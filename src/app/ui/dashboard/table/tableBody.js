d97197ba845a7a6063a020467d1ae840801efe7f
clstyn <fiorenzacelestyn@gmail.com>
2024-02-22 15:33:48 +0700

    fix warning unique key

--- src/app/ui/dashboard/table/tableBody.js
+++ src/app/ui/dashboard/table/tableBody.js
@@ -3,19 +3,19 @@ import Link from "next/link";
 const TableBody = ({ tableData, columns, withAction }) => {
   return (
     <tbody>
-      {tableData.map((data) => {
+      {tableData.map((data, index) => {
         return (
-          <tr key={data.id}>
-            {columns.map(({ accessor }) => {
+          <tr key={index}>
+            {columns.map(({ accessor, index }) => {
               const tData = data[accessor] ? data[accessor] : "——";
               return (
                 <>
                   {accessor === "klien" ? (
-                    <td key={accessor} className="py-4 pl-4 cursor-pointer">
+                    <td key={index} className="py-4 pl-4 cursor-pointer">
                       <Link href={`/report/project-detail`}>{tData}</Link>
                     </td>
                   ) : (
-                    <td key={accessor} className="py-4 pl-4">
+                    <td key={index} className="py-4 pl-4">
                       {tData}
                     </td>
                   )}
