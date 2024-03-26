0f41e31967a68f990c9083e0dc2afc9b95c5bc39
clstyn <fiorenzacelestyn@gmail.com>
2024-03-26 11:18:56 +0700

    fix error each child should unique keyprop

--- src/app/_components/dashboard/table/tableBody.js
+++ src/app/_components/dashboard/table/tableBody.js
@@ -11,30 +11,26 @@ const TableBody = ({ tableData, columns, actionColumn }) => {
       {tableData.map((data) => {
         return (
           <tr key={data.id}>
-            {columns.map(({ accessor, index }) => {
+            {columns.map(({ accessor }, index) => {
               const tData = data[accessor] ? data[accessor] : "——";
-              return (
-                <>
-                  {accessor === "action" ? (
-                    <td key={index} className="py-4 pl-4">
-                      {actionColumn(data, token)}
-                    </td>
-                  ) : accessor === "client_name" ? (
-                    <td key={index} className="py-4 pl-4 cursor-pointer">
-                      <Link href={`/report/${data.id}/project-detail`}>
-                        {tData}
-                      </Link>
-                    </td>
-                  ) : accessor === "end_date" ? (
-                    <td key={index} className="py-4 pl-4">
-                      {formatDate(tData)}
-                    </td>
-                  ) : (
-                    <td key={index} className="py-4 pl-4">
-                      {tData}
-                    </td>
-                  )}
-                </>
+              return accessor === "action" ? (
+                <td key={index} className="py-4 pl-4">
+                  {actionColumn(data, token)}
+                </td>
+              ) : accessor === "client_name" ? (
+                <td key={index} className="py-4 pl-4 cursor-pointer">
+                  <Link href={`/report/${data.id}/project-detail`}>
+                    {tData}
+                  </Link>
+                </td>
+              ) : accessor === "end_date" ? (
+                <td key={index} className="py-4 pl-4">
+                  {formatDate(tData)}
+                </td>
+              ) : (
+                <td key={index} className="py-4 pl-4">
+                  {tData}
+                </td>
               );
             })}
           </tr>
